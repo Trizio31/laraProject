d39160b7db5fa4af8c0410b1034b1cee466dee6a
Trizio <mc.trizio3@gmail.com>
2024-10-31 17:15:52 +0100

    Initial commit

---
+++ database/migrations/2024_02_20_153723_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('username', 20);
+            $table->string('password');
+            $table->integer('role');
+            $table->rememberToken();
+            $table->timestamps();
+
+            //solo per i tecnici 
+            $table->date('birth_date')->nullable();
+            $table->string('specialization')->nullable();
+            $table->foreignId('center')->nullable()->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
